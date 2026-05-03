6f66741fe4fbcc19ede4c7ec5c9a21c0b5a42d72
松本　崇雅 <matsu@matsumototakashimiyabinoMacBook-Air.local>
2026-05-03 20:37:12 +0900

    add config with   env vars

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+CLAUDE_API_KEY = os.environ["CLAUDE_API_KEY"]
+
+THREADS_ACCOUNTS = {
+    "shiba": {
+        "account_id": os.environ["SHIBA_ACCOUNT_ID"],
+        "access_token": os.environ["SHIBA_ACCESS_TOKEN"],
+    }
+}
+
+DATA_SOURCE = "csv"
+REPOST_INTERVAL_DAYS = 7
