4c6612e4493dc314f53abd3d46d826d0d524079c
松本　崇雅 <matsu@matsumototakashimiyabinoMacBook-Air.local>
2026-05-04 12:55:00 +0900

    feat: シュナウザーアカウント追加

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ THREADS_ACCOUNTS = {
     "shiba": {
         "account_id": os.environ["SHIBA_ACCOUNT_ID"],
         "access_token": os.environ["SHIBA_ACCESS_TOKEN"],
-    }
+    },
+    "schnauzer": {
+        "account_id": os.environ["SCHNAUZER_ACCOUNT_ID"],
+        "access_token": os.environ["SCHNAUZER_ACCESS_TOKEN"],
+    },
 }
 
 DATA_SOURCE = "csv"
